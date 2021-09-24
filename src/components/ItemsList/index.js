ddfd61db862f9ba740391ffefbc430dc5d8ff0d1
Arthur Baileys Li <arthurlitovko@gmail.com>
2021-09-24 23:54:20 +0300

    Sort Item List

--- src/components/ItemsList/index.js
+++ src/components/ItemsList/index.js
@@ -8,6 +8,7 @@ const ProductsEndPoint = "https://614db428e3cf1f001712d286.mockapi.io/products";
 class ItemsList extends Component {
 	state = {
 		itemArray: [],
+		order: "Ascending",
 	};
 
 	componentDidMount() {
@@ -16,45 +17,91 @@ class ItemsList extends Component {
 		});
 	}
 
+	onChangeHandler = (evt) => {
+		if (evt.target.name === "sort") {
+			this.sortArray(evt.target.value);
+		}
+
+		if (evt.target.name === "order") {
+			this.setState({ order: "Descending" });
+		}
+
+		if (this.state.order === "Descending") {
+			const reverseArray = this.state.itemArray.reverse();
+			this.setState({ itemArray: reverseArray });
+		}
+	};
+
+	sortArray(method) {
+		let sortedArray = [...this.state.itemArray];
+
+		let sortFunction;
+
+		switch (method) {
+			case "Name":
+				sortFunction = (first, second) => {
+					const firstUpper = first.name.toUpperCase();
+					const secondUpper = second.name.toUpperCase();
+
+					let comparison = 0;
+					if (firstUpper > secondUpper) {
+						comparison = 1;
+					} else if (firstUpper < secondUpper) {
+						comparison = -1;
+					}
+
+					return comparison;
+				};
+				break;
+
+			case "Amount":
+				sortFunction = (first, second) => {
+					return first.amount - second.amount;
+				};
+				break;
+
+			case "Cost":
+				sortFunction = (first, second) => {
+					return first.price - second.price;
+				};
+				break;
+
+			default:
+				break;
+		}
+		sortedArray = sortedArray.sort(sortFunction);
+
+		this.setState({ itemArray: sortedArray });
+	}
+
 	render() {
 		const { itemArray } = this.state;
+
+		const sortOptions = ["Name", "Amount", "Cost"];
 
 		return (
 			<>
 				<header className={s.header}>
 					<h1>Items List</h1>
 
-					<form action="GET" className={s.sort}>
+					<form
+						action="GET"
+						className={s.sort}
+						onChange={this.onChangeHandler}
+					>
 						<p className={s.sort__sort}>
 							Sort by
-							<label>
-								<input
-									type="radio"
-									name="sort"
-									value="Name"
-									className="visually-hidden"
-									checked
-								/>
-								<span>Name</span>
-							</label>
-							<label>
-								<input
-									type="radio"
-									name="sort"
-									className="visually-hidden"
-									value="Amount"
-								/>
-								<span>Amount</span>
-							</label>
-							<label>
-								<input
-									type="radio"
-									name="sort"
-									className="visually-hidden"
-									value="Cost"
-								/>
-								<span>Cost</span>
-							</label>
+							{sortOptions.map((option, index) => (
+								<label key={index}>
+									<input
+										type="radio"
+										name="sort"
+										value={option}
+										className="visually-hidden"
+									/>
+									<span>{option}</span>
+								</label>
+							))}
 						</p>
 
 						<p className={s.sort__order}>
@@ -64,7 +111,7 @@ class ItemsList extends Component {
 									name="order"
 									className="visually-hidden"
 									value="Ascending"
-									checked
+									defaultChecked
 								/>
 								Ascending
 								<svg
@@ -98,7 +145,6 @@ class ItemsList extends Component {
 							</label>
 						</p>
 					</form>
-					{/* {На загальній сторінці продукти можна сортувати по назві, та по кількості даного продукта в наявності.} */}
 					<button type="button" className="button">
 						New Product
 					</button>
