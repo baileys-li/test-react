baec1cfd81c53295ac0c4b2552dabacc29b872cd
Arthur Baileys Li <arthurlitovko@gmail.com>
2021-09-24 17:25:35 +0300

    Delete comments

--- src/components/ItemsList/index.js
+++ src/components/ItemsList/index.js
@@ -19,11 +19,10 @@ class ItemsList extends Component {
 	render() {
 		const { itemArray } = this.state;
 
-		// console.log(itemArray);
+
 		return (
 			<>
 				<h1>Items List</h1>
-				{/* На загальній сторінці продукти можна сортувати по назві, та по кількості даного продукта в наявності.  */}
 
 				<ul className={s.items}>
 					{itemArray.map((item) => (
@@ -39,34 +38,3 @@ class ItemsList extends Component {
 }
 
 export default ItemsList;
-
-{
-	/*
-
-акож повинна бути можливість створення нового продукту з загальної сторінки усіх продуктів.
-При натисканні на кнопку "New Product" відкривається модальне вікно з формою та кнопками "Add" і "Cancel" .
-
-
-Product : {
-	id: 1,
-	imageUrl: 'some url here',
-	name: 'Product name',
-	count: 4,
-	size: {
-		width: 200,
-		height: 200
-	},
-	weight: '200g',
-	comments: [Comment, Comment]
-}
-
-
-Comment: {
-	id: 3,
-	productId: 1,
-	description: 'some text here',
-	date: 14:00 22.08.2021
-}
-
-*/
-}
